8bcab909a75367152a169c908974941cca1de5ba
Caio Torres <caio.torres@pinpeople.com.br>
2022-10-24 10:58:39 -0300

    Great refactor

--- examples/daemon.go
+++ examples/daemon.go
@@ -1,87 +1,83 @@
 package main
 
 import (
-	"fmt"
-	"os"
+    "fmt"
+    "os"
 
-	EventPeople "github.com/pinpeople/event_people_go/lib/event_people"
+    EventPeople "github.com/pinpeople/event_people_go/lib/event_people"
 )
 
 func init() {
-	os.Setenv("RABBIT_EVENT_PEOPLE_APP_NAME", "service")
-	os.Setenv("RABBIT_EVENT_PEOPLE_TOPIC_NAME", "event_people")
-	os.Setenv("RABBIT_EVENT_PEOPLE_VHOST", "event_people")
-	os.Setenv("RABBIT_URL", "amqp://admin:admin@localhost:5672")
-	os.Setenv("RABBIT_FULL_URL", fmt.Sprintf("%s/%s", os.Getenv("RABBIT_URL"), os.Getenv("RABBIT_EVENT_PEOPLE_VHOST")))
+    os.Setenv("RABBIT_EVENT_PEOPLE_APP_NAME", "service_name")
+    os.Setenv("RABBIT_EVENT_PEOPLE_TOPIC_NAME", "event_people")
+    os.Setenv("RABBIT_EVENT_PEOPLE_VHOST", "event_people")
+    os.Setenv("RABBIT_URL", "amqp://admin:admin@localhost:5672")
+    os.Setenv("RABBIT_FULL_URL", fmt.Sprintf("%s/%s", os.Getenv("RABBIT_URL"), os.Getenv("RABBIT_EVENT_PEOPLE_VHOST")))
 
-	EventPeople.Config.Init()
+    EventPeople.Config.Init()
 }
 
 type BodyStructureDaemon struct {
-	Amount int    `json:"amount"`
-	Name   string `json:"name"`
+    Amount int    `json:"amount"`
+    Name   string `json:"name"`
 }
 
 type PrivateMessageDaemon struct {
-	Message string `json:"message"`
-}
-
-type SecondPrivateMessageDaemon struct {
-	Bo string `json:"bo"`
-	Dy string `json:"dy"`
+    Message string `json:"message"`
 }
 
 type CustomEventListener struct {
-	EventPeople.BaseListener
+    EventPeople.BaseListener
 }
 
 func (custom *CustomEventListener) pay(event EventPeople.Event) {
-	var bodyDaemon = BodyStructureDaemon{}
-	event.SetStructBody(&bodyDaemon)
+    var bodyDaemon = BodyStructureDaemon{}
+    event.SetStructBody(&bodyDaemon)
 
-	fmt.Println(fmt.Sprintf("Paid %v for %s ~> %s", bodyDaemon.Amount, bodyDaemon.Name, event.GetRoutingKey()))
-	custom.Success()
+    fmt.Println(fmt.Sprintf("Paid %v for %s ~> %s", bodyDaemon.Amount, bodyDaemon.Name, event.GetRoutingKey()))
+    custom.Success()
 }
 
 func (custom *CustomEventListener) receive(event EventPeople.Event) {
-	var bodyDaemon = BodyStructureDaemon{}
-	event.SetStructBody(&bodyDaemon)
+    var bodyDaemon = BodyStructureDaemon{}
+    event.SetStructBody(&bodyDaemon)
 
-	if bodyDaemon.Amount < 500 {
-		fmt.Println(fmt.Sprintf("[consumer] Got SKIPPED message:\n%d from %s ~> %s", bodyDaemon.Amount, bodyDaemon.Name, event.GetRoutingKey()))
-		custom.Reject()
-		return
-	}
-	fmt.Println(fmt.Sprintf("Received %d from %s ~> %s", bodyDaemon.Amount, bodyDaemon.Name, event.GetRoutingKey()))
-	custom.Success()
+    if bodyDaemon.Amount < 500 {
+        fmt.Println(fmt.Sprintf("[consumer] Got SKIPPED message:\n%d from %s ~> %s", bodyDaemon.Amount, bodyDaemon.Name, event.GetRoutingKey()))
+        custom.Reject()
+        return
+    }
+    fmt.Println(fmt.Sprintf("Received %d from %s ~> %s", bodyDaemon.Amount, bodyDaemon.Name, event.GetRoutingKey()))
+    custom.Success()
 }
 
 func (custom *CustomEventListener) privateChannel(event EventPeople.Event) {
-	var bodyDaemon = PrivateMessageDaemon{}
-	event.SetStructBody(&bodyDaemon)
+    var bodyDaemon = PrivateMessageDaemon{}
+    event.SetStructBody(&bodyDaemon)
 
-	fmt.Println(fmt.Sprintf("[Consumer] Got a private message: %s ~> %s", bodyDaemon.Message, event.GetRoutingKey()))
-	custom.Success()
+    fmt.Println(fmt.Sprintf("[Consumer] Got a private message: \"%s\" ~> %s", bodyDaemon.Message, event.GetRoutingKey()))
+    custom.Success()
 }
 
-func (custom *CustomEventListener) secondPrivateChannel(event EventPeople.Event) {
-	var bodyDaemon = SecondPrivateMessageDaemon{}
-	event.SetStructBody(&bodyDaemon)
+func (custom *CustomEventListener) ignoreMe(event EventPeople.Event) {
+    var bodyDaemon = PrivateMessageDaemon{}
+    event.SetStructBody(&bodyDaemon)
 
-	fmt.Println(fmt.Sprintf("[Consumer] 2 Got a private message: %s ~> %s", fmt.Sprintf("bo: %s -> dy: %s", bodyDaemon.Bo, bodyDaemon.Dy), event.GetRoutingKey()))
-	custom.Success()
+    fmt.Println("This should never be called...")
+    fmt.Println(fmt.Sprintf("Spying on other sustems: \"%s\" ~> %s", bodyDaemon.Message, event.GetRoutingKey()))
+    custom.Success()
 }
 
 func RunDaemon() {
-	custom := new(CustomEventListener)
-	custom.BindEvent(custom.pay, "resource.custom.pay")
-	custom.BindEvent(custom.receive, "resource.custom.receive")
-	custom.BindEvent(custom.privateChannel, "resource.custom.private.service")
-	custom.BindEvent(custom.secondPrivateChannel, "resource.origin.action")
+    custom := new(CustomEventListener)
+    custom.BindEvent(custom.pay, "resource.*.pay")
+    custom.BindEvent(custom.receive, "resource.custom.receive")
+    custom.BindEvent(custom.privateChannel, "resource.custom.private.service")
+    custom.BindEvent(custom.ignoreMe, "resource.custom.ignored.other_service")
 
-	EventPeople.DaemonStart()
+    EventPeople.DaemonStart()
 }
 
 func main() {
-	RunDaemon()
+    RunDaemon()
 }
--- examples/listener.go
+++ examples/listener.go
@@ -1,65 +1,63 @@
 package main
 
 import (
-	"fmt"
-	"os"
-	"time"
+    "fmt"
+    "os"
+    "time"
 
-	EventPeople "github.com/pinpeople/event_people_go/lib/event_people"
+    EventPeople "github.com/pinpeople/event_people_go/lib/event_people"
 )
 
 func init() {
-	os.Setenv("RABBIT_EVENT_PEOPLE_APP_NAME", "service")
-	os.Setenv("RABBIT_EVENT_PEOPLE_TOPIC_NAME", "event_people")
-	os.Setenv("RABBIT_EVENT_PEOPLE_VHOST", "event_people")
-	os.Setenv("RABBIT_URL", "amqp://admin:admin@localhost:5672")
-	os.Setenv("RABBIT_FULL_URL", fmt.Sprintf("%s/%s", os.Getenv("RABBIT_URL"), os.Getenv("RABBIT_EVENT_PEOPLE_VHOST")))
+    os.Setenv("RABBIT_EVENT_PEOPLE_APP_NAME", "service")
+    os.Setenv("RABBIT_EVENT_PEOPLE_TOPIC_NAME", "event_people")
+    os.Setenv("RABBIT_EVENT_PEOPLE_VHOST", "event_people")
+    os.Setenv("RABBIT_URL", "amqp://admin:admin@localhost:5672")
+    os.Setenv("RABBIT_FULL_URL", fmt.Sprintf("%s/%s", os.Getenv("RABBIT_URL"), os.Getenv("RABBIT_EVENT_PEOPLE_VHOST")))
 
-	EventPeople.Config.Init()
+    EventPeople.Config.Init()
 }
 
 func listenerCallback(event EventPeople.Event, context EventPeople.ContextInterface) {
-	msg := event.Body
-	fmt.Println(
-		fmt.Sprintf("EventName: %s \nBody: %s", event.Name, msg),
-	)
-	context.Success()
-	twice <- 1
+    msg := event.Body
+    fmt.Println(
+        fmt.Sprintf("EventName: %s \nBody: %s", event.Name, msg),
+    )
+    context.Success()
+    twice <- 1
 }
 
 var twice = make(chan int)
 
 func RunListener() {
-	EventPeople.ListenTo("resource.custom.*", listenerCallback)
-	EventPeople.ListenTo("resource.origin.*", listenerCallback)
-	<-twice
-	<-twice
-	EventPeople.Config.CloseConnection()
+    EventPeople.ListenTo("resource.custom.*", listenerCallback)
+    EventPeople.ListenTo("resource.origin.*", listenerCallback)
+    <-twice
+    <-twice
+    EventPeople.Config.CloseConnection()
 }
 
 var once = make(chan int)
 
 func main() {
-	// RunListener()
+    var eventName = "payment.payments.pay.service"
 
-	var eventName = "payment.payments.pay.service"
+    EventPeople.ListenTo(eventName, func(event EventPeople.Event, context EventPeople.ContextInterface) {
+        msg := event.Body
 
-	EventPeople.ListenTo(eventName, func(event EventPeople.Event, context EventPeople.ContextInterface) {
-		msg := event.Body
+        fmt.Println("")
+        fmt.Println(fmt.Sprintf("  - Received the %s message from %s:", event.Name, event.Headers.Origin))
+        fmt.Println(fmt.Sprintf("     Message: %s", msg))
+        fmt.Println("")
+        context.Success()
+    })
 
-		fmt.Println("")
-		fmt.Println(fmt.Sprintf("  - Received the %s message from %s:", event.Name, event.Headers.Origin))
-		fmt.Println(fmt.Sprintf("     Message: %s", msg))
-		fmt.Println("")
-		context.Success()
-	})
+    defer EventPeople.Config.CloseConnection()
 
-	defer EventPeople.Config.CloseConnection()
+    go func() {
+        time.Sleep(15 * time.Second)
+        once <- 1
+    }()
 
-	go func() {
-		time.Sleep(15 * time.Second)
-		once <- 1
-	}()
-
-	<-once
+    <-once
 }
