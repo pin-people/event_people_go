4523bbb7527041e90e34e16c3d2edd6837657ad7
Willian Sanches <willianbvsanches@gmail.com>
2022-10-28 15:49:43 -0300

    fix the problems
    Co-authored-by: Caio Torres <efreesen@users.noreply.github.com>

--- lib/event_people/rabbit_queue.go
+++ lib/event_people/rabbit_queue.go
@@ -1,81 +1,80 @@
 package EventPeople
 
 import (
-    "encoding/json"
-    "fmt"
-    "os"
-    "strings"
+	"encoding/json"
+	"fmt"
+	"os"
+	"strings"
 
-    amqp "github.com/rabbitmq/amqp091-go"
+	amqp "github.com/rabbitmq/amqp091-go"
 )
 
 type QueueInterface interface {
-    Subscribe(routingKey string, callback Callback)
-    SubscribeWithChannel(channel ContextInterface, routingKey string, callback Callback)
-    Init(channel ContextInterface)
-    QueueOptions()
-    QueueName(routingKey string)
-    queueBind()
-    exchangeBind()
-    callback()
+	Subscribe(routingKey string, callback Callback)
+	SubscribeWithChannel(channel ContextInterface, routingKey string, callback Callback)
+	Init(channel ContextInterface)
+	QueueOptions()
+	QueueName(routingKey string)
+	queueBind()
+	exchangeBind()
+	callback()
 }
 
 type Queue struct {
-    amqpQueue *amqp.Queue
-    channel   *amqp.Channel
-    QueueInterface
+	amqpQueue *amqp.Queue
+	channel   *amqp.Channel
+	QueueInterface
 }
 
 func (queue *Queue) Init(channel *amqp.Channel) {
-    queue.channel = channel
+	queue.channel = channel
 }
 
 func (queue *Queue) Subscribe(routingKey string, callback Callback) {
-    queue.createQueueAndBind(routingKey, callback)
+	queue.createQueueAndBind(routingKey, callback)
 }
 
 func (queue *Queue) GetConsumers() int {
-    return queue.amqpQueue.Consumers
+	return queue.amqpQueue.Consumers
 }
 
 func (queue *Queue) QueueName(routingKey string) string {
-    return queue.amqpQueue.Name
+	return queue.amqpQueue.Name
 }
 
 func (queue *Queue) callback(deliveries <-chan amqp.Delivery, callback Callback) {
-    for delivery := range deliveries {
-        var eventMessage Event
-        json.Unmarshal(delivery.Body, &eventMessage)
+	for delivery := range deliveries {
+		var eventMessage Event
+		json.Unmarshal(delivery.Body, &eventMessage)
 
-        eventMessage.Name = eventMessage.Headers.AppName
-        eventMessage.SchemaVersion = eventMessage.Headers.SchemaVersion
+		eventMessage.Name = eventMessage.Headers.AppName
+		eventMessage.SchemaVersion = eventMessage.Headers.SchemaVersion
 
-        callback(eventMessage, NewContext(&delivery))
-    }
+		callback(eventMessage, NewContext(&delivery))
+	}
 }
 
 func (queue *Queue) createQueue(routingKey string) {
-    localQueue, err := queue.channel.QueueDeclare(routingKey, true, false, false, false, nil)
-    FailOnError(err, "Failed to declare a queue")
-    queue.amqpQueue = &localQueue
+	localQueue, err := queue.channel.QueueDeclare(routingKey, true, false, false, false, nil)
+	FailOnError(err, "Failed to declare a queue")
+	queue.amqpQueue = &localQueue
 }
 
 func (queue *Queue) exchangeBind(queueName string, routingKey string) {
-    err := queue.channel.ExchangeDeclarePassive(os.Getenv("RABBIT_EVENT_PEOPLE_TOPIC_NAME"), "topic", true, false, false, false, nil)
-    FailOnError(err, "Failed to declare an exchange")
+	err := queue.channel.ExchangeDeclarePassive(os.Getenv("RABBIT_EVENT_PEOPLE_TOPIC_NAME"), "topic", true, false, false, false, nil)
+	FailOnError(err, "Failed to declare an exchange")
 
-    queue.channel.QueueBind(queueName, routingKey, os.Getenv("RABBIT_EVENT_PEOPLE_TOPIC_NAME"), false, nil)
-    FailOnError(err, "Failed to bind queue to exchange")
+	queue.channel.QueueBind(queueName, routingKey, os.Getenv("RABBIT_EVENT_PEOPLE_TOPIC_NAME"), false, nil)
+	FailOnError(err, "Failed to bind queue to exchange")
 }
 
 func (queue *Queue) createQueueAndBind(routingKey string, callback Callback) {
-    eventNameSplited := strings.Split(routingKey, ".")
-    eventNameSplited[3] = "all"
-    queueName := os.Getenv("RABBIT_EVENT_PEOPLE_APP_NAME") + "-" + strings.Join(eventNameSplited, ".")
-    queue.createQueue(queueName)
-    queue.exchangeBind(queueName, routingKey)
-    messages, err := queue.channel.Consume(queueName, "", false, false, false, false, nil)
-    FailOnError(err, "Failed to consume a queue")
-    go queue.callback(messages, callback)
-    fmt.Printf("Event People consuming %s Queue!\n", queue.amqpQueue.Name)
+	eventNameSplited := strings.Split(routingKey, ".")
+	queueName := os.Getenv("RABBIT_EVENT_PEOPLE_APP_NAME") + "-" + strings.Join(eventNameSplited, ".")
+	queue.createQueue(queueName)
+	queue.exchangeBind(queueName, routingKey)
+	messages, err := queue.channel.Consume(queueName, queueName, false, false, false, false, nil)
+	FailOnError(err, "Failed to consume a queue")
+	go queue.callback(messages, callback)
+	fmt.Printf("Event People consuming %s Queue!\n", queue.amqpQueue.Name)
 }
--- lib/event_people/rabbit_topic.go
+++ lib/event_people/rabbit_topic.go
@@ -1,38 +1,37 @@
 package EventPeople
 
 import (
-    "context"
-    "fmt"
-    "os"
-    "time"
+	"context"
+	"fmt"
+	"os"
+	"time"
 
-    amqp "github.com/rabbitmq/amqp091-go"
+	amqp "github.com/rabbitmq/amqp091-go"
 )
 
 type Topic struct {
-    channel *amqp.Channel
+	channel *amqp.Channel
 }
 
 func (topic *Topic) Init(channel *amqp.Channel) {
-    topic.channel = channel
+	topic.channel = channel
 }
 
 func (topic *Topic) Produce(event Event) {
-    message := amqp.Publishing{
-        DeliveryMode: amqp.Persistent,
-        Timestamp:    time.Now(),
-        ContentType:  "text/plain",
-        Body:         []byte(event.Payload()),
-    }
-    ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
-    defer cancel()
-    fmt.Printf("Producing message to %s!\n", event.Name)
-    err := topic.channel.PublishWithContext(ctx, os.Getenv("RABBIT_EVENT_PEOPLE_TOPIC_NAME"), event.Name, false, false, message)
-    fmt.Printf("Message sent to %s!\n", event.Name)
-
-    FailOnError(err, "Error on publish message")
+	message := amqp.Publishing{
+		DeliveryMode: amqp.Persistent,
+		Timestamp:    time.Now(),
+		ContentType:  "text/plain",
+		Body:         []byte(event.Payload()),
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	fmt.Printf("Producing message to %s!\n", event.GetRoutingKey())
+	err := topic.channel.PublishWithContext(ctx, os.Getenv("RABBIT_EVENT_PEOPLE_TOPIC_NAME"), event.GetRoutingKey(), false, false, message)
+	FailOnError(err, "Error on publish message")
+	fmt.Printf("Message sent to %s!\n", event.GetRoutingKey())
+	defer cancel()
 }
 
 func (topic *Topic) GetChannel() *amqp.Channel {
-    return topic.channel
+	return topic.channel
 }
