9df6d22ad9d97d8de8b51cdb9bc60f0ea0add07e
willianbvsanches13 <33604112+willianbvsanches13@users.noreply.github.com>
2023-06-06 22:25:33 -0300

    fix: add mutex to delivery channel to fix race confition error

--- lib/event_people/rabbit_context.go
+++ lib/event_people/rabbit_context.go
@@ -1,8 +1,12 @@
 package EventPeople
 
 import (
+    "sync"
+
 	amqp "github.com/rabbitmq/amqp091-go"
 )
+
+var mutex sync.Mutex
 
 type RabbitContext struct {
     ContextInterface
@@ -15,18 +19,22 @@ func (context *RabbitContext) Initialize(delivery *amqp.Delivery) {
 
 func (context *RabbitContext) Success() {
     context.delivery.Ack(false)
+    mutex.Unlock()
 }
 
 func (context *RabbitContext) Fail() {
     context.delivery.Nack(false, true)
+    mutex.Unlock()
 }
 
 func (context *RabbitContext) Reject() {
     context.delivery.Reject(false)
+    mutex.Unlock()
 }
 
 func NewContext(delivery *amqp.Delivery) *RabbitContext {
     context := new(RabbitContext)
     context.Initialize(delivery)
+    mutex.Lock()
     return context
 }
