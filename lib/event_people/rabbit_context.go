8bb5710bd1e6dea62b5fd9edc76fd850f99565f0
Willian Sanches <willianbvsanches@gmail.com>
2023-06-12 11:34:12 -0300

    fix: add mutex to context

--- lib/event_people/rabbit_context.go
+++ lib/event_people/rabbit_context.go
@@ -1,32 +1,40 @@
 package EventPeople
 
 import (
+	"sync"
+
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
+var mutex sync.Mutex
+
 type RabbitContext struct {
-    ContextInterface
-    delivery *amqp.Delivery
+	ContextInterface
+	delivery *amqp.Delivery
 }
 
 func (context *RabbitContext) Initialize(delivery *amqp.Delivery) {
-    context.delivery = delivery
+	context.delivery = delivery
 }
 
 func (context *RabbitContext) Success() {
-    context.delivery.Ack(false)
+	context.delivery.Ack(false)
+	mutex.Unlock()
 }
 
 func (context *RabbitContext) Fail() {
-    context.delivery.Nack(false, true)
+	context.delivery.Nack(false, true)
+	mutex.Unlock()
 }
 
 func (context *RabbitContext) Reject() {
-    context.delivery.Reject(false)
+	context.delivery.Reject(false)
+	mutex.Unlock()
 }
 
 func NewContext(delivery *amqp.Delivery) *RabbitContext {
-    context := new(RabbitContext)
-    context.Initialize(delivery)
-    return context
+	context := new(RabbitContext)
+	context.Initialize(delivery)
+	mutex.Lock()
+	return context
 }
